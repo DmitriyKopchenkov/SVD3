8b4d89c5ab70738eaf8ce7a23c9442492e1f7b27
DmitriyKopchenkov <103284097+DmitriyKopchenkov@users.noreply.github.com>
2022-04-21 23:22:03 +0300

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Roman","Longer",(byte) 25);
+        userService.saveUser("Podzalupny","Cottagecheese",(byte) 30);
+        userService.saveUser("Cheap","Whore",(byte) 55);
+        userService.saveUser("Darima", "Soplezhuyka",(byte) 14);
+
+        System.out.println(userService.getAllUsers().toString());
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
